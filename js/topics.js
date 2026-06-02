2196f1a80a8c6cd35644e69a898c07a8fcecacec
root <root@HRI-PC104.localdomain>
2026-06-02 18:16:07 +0900

    topics.js 修正

--- js/topics.js
+++ js/topics.js
@@ -6,177 +6,102 @@
 export const TOPICS = {
 
   animal: [
-
-    {
-      word1: "犬",
-      word2: "猫"
-    },
-
-    {
-      word1: "ライオン",
-      word2: "トラ"
-    },
-
-    {
-      word1: "イルカ",
-      word2: "クジラ"
-    },
-
-    {
-      word1: "パンダ",
-      word2: "コアラ"
-    },
-
-    {
-      word1: "ウサギ",
-      word2: "ハムスター"
-    }
-
+    { word1: "犬", word2: "猫" },
+    { word1: "ライオン", word2: "トラ" },
+    { word1: "イルカ", word2: "クジラ" },
+    { word1: "パンダ", word2: "コアラ" },
+    { word1: "ウサギ", word2: "ハムスター" },
+    { word1: "ハムスター", word2: "モルモット" },
+    { word1: "ペンギン", word2: "アザラシ" }
   ],
 
   food: [
-
-    {
-      word1: "カレー",
-      word2: "シチュー"
-    },
-
-    {
-      word1: "ラーメン",
-      word2: "うどん"
-    },
-
-    {
-      word1: "寿司",
-      word2: "刺身"
-    },
-
-    {
-      word1: "焼肉",
-      word2: "しゃぶしゃぶ"
-    },
-
-    {
-      word1: "ハンバーグ",
-      word2: "ステーキ"
-    }
-
+    { word1: "カレー", word2: "シチュー" },
+    { word1: "ラーメン", word2: "うどん" },
+    { word1: "寿司", word2: "刺身" },
+    { word1: "焼肉", word2: "しゃぶしゃぶ" },
+    { word1: "ハンバーグ", word2: "ステーキ" },
+    { word1: "ラーメン", word2: "つけ麺" },
+    { word1: "唐揚げ", word2: "とんかつ" },
+    { word1: "餃子", word2: "焼売" },
+    { word1: "お好み焼き", word2: "たこ焼き" },
+    { word1: "チャーハン", word2: "ピラフ" },
+    { word1: "うどん", word2: "そば" }
   ],
 
   drink: [
-
-    {
-      word1: "コーラ",
-      word2: "サイダー"
-    },
-
-    {
-      word1: "コーヒー",
-      word2: "紅茶"
-    },
-
-    {
-      word1: "牛乳",
-      word2: "豆乳"
-    },
-
-    {
-      word1: "緑茶",
-      word2: "麦茶"
-    },
-
-    {
-      word1: "カルピス",
-      word2: "ヤクルト"
-    }
-
+    { word1: "コーラ", word2: "サイダー" },
+    { word1: "コーヒー", word2: "紅茶" },
+    { word1: "牛乳", word2: "豆乳" },
+    { word1: "緑茶", word2: "麦茶" },
+    { word1: "カルピス", word2: "ヤクルト" },
+    { word1: "ビール", word2: "ハイボール" },
+    { word1: "日本酒", word2: "焼酎" },
+    { word1: "オレンジジュース", word2: "リンゴジュース" }
   ],
 
   sports: [
-
-    {
-      word1: "野球",
-      word2: "サッカー"
-    },
-
-    {
-      word1: "テニス",
-      word2: "卓球"
-    },
-
-    {
-      word1: "バスケットボール",
-      word2: "バレーボール"
-    },
-
-    {
-      word1: "柔道",
-      word2: "空手"
-    },
-
-    {
-      word1: "ゴルフ",
-      word2: "ゲートボール"
-    }
-
+    { word1: "野球", word2: "サッカー" },
+    { word1: "テニス", word2: "卓球" },
+    { word1: "バスケットボール", word2: "バレーボール" },
+    { word1: "柔道", word2: "空手" },
+    { word1: "ゴルフ", word2: "ゲートボール" }
   ],
 
   game: [
-
-    {
-      word1: "マリオ",
-      word2: "ソニック"
-    },
-
-    {
-      word1: "ポケモン",
-      word2: "デジモン"
-    },
-
-    {
-      word1: "スプラトゥーン",
-      word2: "フォートナイト"
-    },
-
-    {
-      word1: "Minecraft",
-      word2: "Terraria"
-    },
-
-    {
-      word1: "ドラゴンクエスト",
-      word2: "ファイナルファンタジー"
-    }
-
+    { word1: "マリオ", word2: "ソニック" },
+    { word1: "ポケモン", word2: "デジモン" },
+    { word1: "スプラトゥーン", word2: "フォートナイト" },
+    { word1: "Minecraft", word2: "Terraria" },
+    { word1: "ドラゴンクエスト", word2: "ファイナルファンタジー" }
   ],
 
   anime: [
+    { word1: "ドラえもん", word2: "クレヨンしんちゃん" },
+    { word1: "ワンピース", word2: "ナルト" },
+    { word1: "鬼滅の刃", word2: "呪術廻戦" },
+    { word1: "名探偵コナン", word2: "金田一少年の事件簿" },
+    { word1: "進撃の巨人", word2: "東京喰種" }
+  ],
 
-    {
-      word1: "ドラえもん",
-      word2: "クレヨンしんちゃん"
-    },
+  school: [
+    { word1: "数学", word2: "理科" },
+    { word1: "国語", word2: "社会" },
+    { word1: "先生", word2: "校長" },
+    { word1: "部活", word2: "委員会" },
+    { word1: "高校", word2: "大学" },
+    { word1: "修学旅行", word2: "遠足" }
+  ],
 
-    {
-      word1: "ワンピース",
-      word2: "ナルト"
-    },
+  vehicle: [
+    { word1: "車", word2: "バイク" },
+    { word1: "飛行機", word2: "新幹線" },
+    { word1: "船", word2: "フェリー" },
+    { word1: "自転車", word2: "キックボード" },
+    { word1: "タクシー", word2: "バス" }
+  ],
 
-    {
-      word1: "鬼滅の刃",
-      word2: "呪術廻戦"
-    },
+  appliance: [
+    { word1: "冷蔵庫", word2: "冷凍庫" },
+    { word1: "洗濯機", word2: "乾燥機" },
+    { word1: "テレビ", word2: "プロジェクター" },
+    { word1: "電子レンジ", word2: "オーブン" },
+    { word1: "パソコン", word2: "タブレット" }
+  ],
 
-    {
-      word1: "名探偵コナン",
-      word2: "金田一少年の事件簿"
-    },
+  sns: [
+    { word1: "LINE", word2: "Instagram" },
+    { word1: "X", word2: "Threads" },
+    { word1: "TikTok", word2: "YouTube" },
+    { word1: "Discord", word2: "Skype" },
+    { word1: "Facebook", word2: "mixi" }
+  ],
 
-    {
-      word1: "進撃の巨人",
-      word2: "東京喰種"
-    }
-
+  love: [
+    { word1: "告白", word2: "プロポーズ" },
+    { word1: "恋人", word2: "親友" },
+    { word1: "デート", word2: "旅行" },
+    { word1: "片思い", word2: "失恋" }
   ]
 
 };
